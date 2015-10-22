78a627a97d4ec372b9bc55551040ff4f920101c5
UTFPR <UTFPR@192.168.16.240>
2015-10-22 11:34:30 -0200

    Melhora com relação ao vetor

--- src/br/edu/utfpr/dao/PessoaDaoVetor.java
+++ src/br/edu/utfpr/dao/PessoaDaoVetor.java
@@ -3,9 +3,10 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-package dao;
 
-import modelo.Pessoa;
+
+import br.edu.utfpr.dao.Dao;
+import br.edu.utfpr.modelo.Pessoa;
 
 /**
  *
